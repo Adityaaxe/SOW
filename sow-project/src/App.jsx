eeb712a5302119ad48485171a0d47d01144141c4
Adityaaxe <aditya.singh2021@sait.ac.in>
2025-06-01 11:54:30 +0530

    commit

--- sow-project/src/App.jsx
+++ sow-project/src/App.jsx
@@ -2,16 +2,6 @@ import AppRouter from "./Router/AppRouter";
 import React, { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    const setVh = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-    };
-
-    setVh();
-    window.addEventListener('resize', setVh);
-    return () => window.removeEventListener('resize', setVh);
-  }, []);
   return <AppRouter />;
 }
 
--- sow-project/src/Router/AppRouter.jsx
+++ sow-project/src/Router/AppRouter.jsx
@@ -4,18 +4,7 @@ import Terms from "../Pages/Terms";
 import PriceListPage from "../Pages/Pricelist";
 
 function App() {
-  useEffect(() => {
-    const setVh = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-    };
-
-    setVh();
-    window.addEventListener('resize', setVh);
-    return () => window.removeEventListener('resize', setVh);
-  }, []);
-
-
+  
   return (
     <Router>
       <Routes>
